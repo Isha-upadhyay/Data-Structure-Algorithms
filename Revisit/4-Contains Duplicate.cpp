d8957af36fdf2fcd4cbd4951eb4aa7855c912907
Isha Upadhyay <148354274+Isha-upadhyay@users.noreply.github.com>
2025-11-29 03:15:14 +0530

    Create 4-Contains Duplicate.cpp

---
+++ Revisit/4-Contains Duplicate.cpp
@@ -0,0 +1,11 @@
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int, int> mp;
+
+        for(int num:nums){
+            mp[num]++;
+
+            if(mp[num] > 1) return true;
+        }
+        return false;
+    }
+};
