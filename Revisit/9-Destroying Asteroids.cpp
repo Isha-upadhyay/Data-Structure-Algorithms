59f99637fa7ac6df1f723b91d2ed0db7f455043c
Isha Upadhyay <148354274+Isha-upadhyay@users.noreply.github.com>
2026-03-01 17:28:27 +0530

    Create 9-Destroying Asteroids.cpp

---
+++ Revisit/9-Destroying Asteroids.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool asteroidsDestroyed(int mass, vector<int>& asteroids) {
+        long long currMass = mass; 
+        sort(asteroids.begin(), asteroids.end());
+
+        for(int asteroid : asteroids){
+            if(currMass< asteroid) {
+                return false;
+            }
+
+            currMass += asteroid;
+        }
+        return true;
+    }
+};
+
+
+
+
+
+
